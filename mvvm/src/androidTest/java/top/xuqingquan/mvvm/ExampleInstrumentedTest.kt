7b648a2950bb4b5e119112341374ee340033734f
许清泉 <xuqingquan1995@gmail.com>
2019-05-13 21:52:35 +0800

    更改MVVM模块包名

--- mvvm/src/androidTest/java/top/xuqingquan/mvvm/ExampleInstrumentedTest.kt
+++ mvvm/src/androidTest/java/top/xuqingquan/mvvm/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package top.xuqingquan
+package top.xuqingquan.mvvm
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
--- mvvm/src/test/java/top/xuqingquan/mvvm/ExampleUnitTest.kt
+++ mvvm/src/test/java/top/xuqingquan/mvvm/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package top.xuqingquan
+package top.xuqingquan.mvvm
 
 import org.junit.Test
 
