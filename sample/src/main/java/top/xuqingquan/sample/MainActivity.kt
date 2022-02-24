b87edceaf37570e8d5bd2f44259dd89c1eed4922
许清泉 <xuqingquan1995@gmail.com>
2022-02-24 20:10:32 +0800

    更新demo

--- sample/src/main/java/top/xuqingquan/sample/MainActivity.kt
+++ sample/src/main/java/top/xuqingquan/sample/MainActivity.kt
@@ -1,19 +1,19 @@
 package top.xuqingquan.sample
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import top.xuqingquan.mvvm.view.activity.BaseActivity
 import top.xuqingquan.sample.databinding.ActivityMainBinding
+import top.xuqingquan.utils.toast
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
+    private val TAG = "MainActivity"
 
     override fun getVM() = MainViewModel()
 
-    override fun getLayoutId() = R.layout.activity_main
-
-    override fun initData(savedInstanceState: Bundle?) {
-        viewModel!!.test.observe(this, Observer {
-            binding.data = it
-        })
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel!!.test.observe(this) {
+            toast("testhh--->${it}")
+        }
     }
 }
--- sample/src/main/java/top/xuqingquan/sample/MainRepository.kt
+++ sample/src/main/java/top/xuqingquan/sample/MainRepository.kt
@@ -1,15 +1,27 @@
 package top.xuqingquan.sample
 
-import top.xuqingquan.base.model.repository.BaseRepository
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import top.xuqingquan.mvvm.extension.launch
+import top.xuqingquan.mvvm.model.repository.BaseRepository
 import top.xuqingquan.utils.Timber
 
 /**
  * Created by 许清泉 on 2019-07-01 15:27
  */
 class MainRepository : BaseRepository() {
-
+    private val TAG = "MainRepository"
     fun test() {
         Timber.d("mRepositoryManager==null====>${mRepositoryManager == null}")
+        launch(context = Dispatchers.Main, tryBlock = {
+            val l=listOf("123","345")
+            Log.e(TAG, "test: ${l[3]}")
+        }, catchBlock = {e ->
+            Log.e(TAG, "test: catchBlock")
+            e.printStackTrace()
+        }, finallyBlock = {
+            Log.e(TAG, "test: finallyBlock")
+        })
     }
 
 }
